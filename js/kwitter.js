5c4a82bcc2f24dca03a991df037fd6ac956eba05
dakshc3109 <78908644+dakshc3109@users.noreply.github.com>
2021-06-22 21:10:04 +0530

    Update kwitter.js

--- js/kwitter.js
+++ js/kwitter.js
@@ -1,5 +1,10 @@
 function logIn(){
     var user_name = document.getElementById("user_name").value;
-    localStorage.setItem("user_name", user_name);
-    window.location = "kwitter_room.html";
+    if(user_name == ""){
+        alert("Enter your name")
+    }
+    else{
+        localStorage.setItem("user_name", user_name);
+        window.location = "kwitter_room.html";
+    }
 }
